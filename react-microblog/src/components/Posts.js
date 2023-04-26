47ed41e4ec768a0be6588f781329fe6a61c32536
Ivan <ikladinov@gmail.com>
2023-04-26 12:45:41 -0400

    explorePage

--- react-microblog/src/components/Posts.js
+++ react-microblog/src/components/Posts.js
@@ -1,25 +1,38 @@
 
 import { useState, useEffect } from 'react';
 import Spinner from 'react-bootstrap/Spinner';
+import { useApi } from '../contexts/ApiProvider';
 import Post from './Post';
 
-const BASE_API_URL = process.env.REACT_APP_BASE_API_URL;
-
-export default function Posts() {
+export default function Posts({ content }) {
   const [posts, setPosts] = useState();
+  const api = useApi();
+
+  let url;
+  switch (content) {
+    case 'feed':
+    case undefined:
+      url = '/feed';
+      break;
+    case 'explore':
+      url = '/posts';
+      break
+    default:
+      url = `/users/${content}/posts`;
+      break;
+  }
 
   useEffect(() => {
     (async () => {
-      const response = await fetch(BASE_API_URL + '/api/feed');
+      const response = await api.get(url);
       if (response.ok) {
-        const results = await response.json();
-        setPosts(results.data);
+        setPosts(response.body.data);
       }
       else {
         setPosts(null);
       }
     })();
-  }, []);
+  }, [api, url]);
 
   return (
     <>

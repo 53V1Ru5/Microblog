d09c52207f70c7bd4bc076992ee765c91f1a33ef
Ivan <ikladinov@gmail.com>
2023-04-26 17:05:22 -0400

    authentication

--- react-microblog/src/App.js
+++ react-microblog/src/App.js
@@ -8,8 +8,9 @@ import UserPage from './pages/UserPage';
 import LoginPage from './pages/LoginPage';
 import RegistrationPage from './pages/RegistrationPage';
 import FlashProvider from './contexts/FlashProvider';
-
-
+import UserProvider from './contexts/UserProvider';
+import PrivateRoute from './components/PrivateRoute';
+import PublicRoute from './components/PublicRoute';
 
 export default function App() {
   return (
@@ -17,15 +18,27 @@ export default function App() {
       <BrowserRouter>
         <FlashProvider>
           <ApiProvider>
-            <Header />
-            <Routes>
-            <Route path="/" element={<FeedPage />} />
-            <Route path="/explore" element={<ExplorePage />} />
-            <Route path="/user/:username" element={<UserPage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/register" element={<RegistrationPage />} />
-            <Route path="*" element={<Navigate to="/" />} />
-            </Routes>
+            <UserProvider>
+              <Header />
+              <Routes>
+                <Route path="/login" element={
+                  <PublicRoute><LoginPage /></PublicRoute>
+                } />
+                <Route path="/register" element={
+                  <PublicRoute><RegistrationPage /></PublicRoute>
+                } />
+                <Route path="*" element={
+                  <PrivateRoute>
+                    <Routes>
+                      <Route path="/" element={<FeedPage />} />
+                      <Route path="/explore" element={<ExplorePage />} />
+                      <Route path="/user/:username" element={<UserPage />} />
+                      <Route path="*" element={<Navigate to="/" />} />
+                    </Routes>
+                  </PrivateRoute>
+                } />
+              </Routes>
+            </UserProvider>
           </ApiProvider>
         </FlashProvider>
       </BrowserRouter>
